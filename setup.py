7fbcae78441c3b99dcb62fc718af68074fe3b666
armoken <alex.armoken@gmail.com>
2024-10-09 23:17:16 +0200

    Add first version

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='dmenu-usbguard',
+      version='1.0',
+      # Modules to import from other scripts:
+      packages=find_packages(),
+      # Executables
+      scripts=["dmenu-usbguard"])
